ea1622d24e4002680cc09a9132f0262c5bb36297
gyeolse <threewave@kakao.com>
2020-03-09 19:05:29 +0900

    first commit

--- boj_2309.cpp
+++ boj_2309.cpp
@@ -14,15 +14,16 @@ int main(void){
 
     sort(input,input+9);
 
-    for(int i=0;;i++){
-        for(int j=0;j<i;j++){
-            if((sum-input[i]-input[j])==0){
+    for(int i=0;i<9;i++){
+        for(int j=i+1;j<9;j++){
+            if(sum-input[i]-input[j]==100){
+                for(int k=0;k<9;k++){
+                    if(i==k||j==k) continue;
+                    cout<<input[k]<<'\n';
+                }
                 return 0;
             }
-
         }
     }
 
-    
-    return 0;   
 }
--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(void){
-    cout<<"넌 왜 한글이 안되니??"<<"\n";
-    return 0;
-    
-}
