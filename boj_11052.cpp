d7f4355e2d3d691b93db547b6d9480b0e1a9dec9
gyeolse <threewave@kakao.com>
2020-04-05 20:28:29 +0900

    DP.

---
+++ boj_11052.cpp
@@ -0,0 +1,27 @@
+//카드 구매하기
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//1. 점화식 세우기. D[i] = max(P[i]+D[i-j])
+//2. 작게 쪼개기 Top-Down || Bottom Up
+
+int main(void){
+    int N;
+    cin>>N;
+    vector<int> a(N+1); //a. 크기 N+1. 가격 저장
+    for(int i=1;i<=N;i++){
+        cin>>a[i];
+    }
+    vector<int> d(N+1); //d. 최댓값 저장.
+    for(int i=1;i<=N;i++){
+        for(int j=1;j<=i;j++){
+            d[i] = max(d[i],d[i-j]+a[j]);
+        }
+    }
+    cout<<d[N]<<'\n';
+
+    return 0;
+}
