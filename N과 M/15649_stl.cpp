aed0514da126691352be6d1d334b5b0e0409017b
gyeolse <threewave@kakao.com>
2020-11-15 20:19:14 +0900

    next_permutation을 이용해서 순열 문제풀이

--- N과 M/15649_stl.cpp
+++ N과 M/15649_stl.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 /**
  * 자연수 N과 M이 주어지고, 한 줄에 하나씩 문제의 조건에 만족하는 수열을 출력
-*/
+ * 
+ * (sol)
+ * 중복되는 경우, 중복 처리를 해주었다.
+ * loop에 들어가기 전의 temp 벡터, loop에 들어간 후 lptemp 벡터가 서로 같은지 비교해주었다. 
+ */
 int main(){
     int m,n;
     int arr[10];
@@ -13,7 +17,7 @@ int main(){
     for(int i=0;i<n;i++){
         arr[i]=i+1;
     }
-    
+
     vector<int> temp;
     do {
         vector<int> lptemp;
