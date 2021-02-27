5fa90cbf83d35c73e2be3385e1ac23a82fa85b0a
gyeolse <threewave@kakao.com>
2021-02-27 23:54:59 +0900

    [ADD] 문제 풀이

--- 11_DP/14002.cpp
+++ 11_DP/14002.cpp
@@ -5,6 +5,16 @@ int arr[1001];
 int dp[1001];
 int pastIdx[1001];
 
+void func(int num) {
+    if (pastIdx[num] == -1) {
+        cout << arr[num] << " ";
+        return;
+    }
+
+    int next = pastIdx[num];
+    func(next);
+    cout << arr[num] << " ";
+}
 int main() {
     int n;
     cin >> n;
@@ -13,6 +23,8 @@ int main() {
     }
 
     fill_n(dp, n, 1);
+    fill_n(pastIdx, n, -1);
+
     for (int i = 0;i < n;i++) {
         for (int j = 0;j < n;j++) {
             if (arr[i] > arr[j]) {
@@ -24,7 +36,15 @@ int main() {
         }
     }
 
+    int s_ = *max_element(dp, dp + n);
+    int idx = 0;
+    for (int i = 0;i < n;i++) {
+        if (s_ == dp[i]) idx = i;
+    }
+
     cout << *max_element(dp, dp + n) << "\n";
+
+    func(idx);
 
     return 0;
 }
---
+++ 5_브루트포스/6603.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int num;
+int arr[50];
+int input[50];
+
+void func(int n, int idx) {
+    if (idx == 6) {
+        for (int i = 0;i < 6;i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    for (int i = n;i < num;i++) {
+        arr[idx] = input[i];
+        func(i + 1, idx + 1);
+    }
+}
+
+int main(void) {
+    while (1) {
+        cin >> num;
+        if (num == 0) break;
+        for (int i = 0;i < num;i++) {
+            cin >> input[i];
+        }
+        func(0, 0);
+        cout << "\n";
+    }
+    return 0;
+}
